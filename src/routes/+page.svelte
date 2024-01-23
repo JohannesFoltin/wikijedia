ecf870c19ef2daf0378984a1a5a4bd19b97aae77
Johannes <Johannes.Foltin@gmx.de>
2024-01-23 15:43:18 +0100

    switched to editorjs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,60 +1,31 @@
 <script>
-    import lex from "lexical";
-    const { createEditor, LineBreakNode, ParagraphNode, TextNode } = lex;
     import { onMount } from "svelte";
-    import lexicalList from "@lexical/list";
 
-    const { ListItemNode, ListNode } = lexicalList;
+    import { createEditor } from "svelte-editorjs";
+    import Header from '@editorjs/header';
 
-    import lexicalRichtText from "@lexical/rich-text";
-    import lexicalPlainText from "@lexical/plain-text";
-    const { registerPlainText } = lexicalPlainText;
-
-    let editorElement;
-
-    onMount(() => {
-        const config = {
-            nodes: [TextNode, LineBreakNode, ParagraphNode],
-            onError: console.error,
-        };
-        const editor = createEditor(config);
-        editor.update(() => {
-            editor.setRootElement(editorElement);
-        });
-        registerPlainText(editor);
-        console.log(editor.getEditorState());
-        console.log(editor.isEditable());
-        editor.update(() => {
-            // Get the RootNode from the EditorState
-            const root = lex.$getRoot();
-
-            // Get the selection from the EditorState
-            const selection = lex.$getSelection();
-
-            // Create a new ParagraphNode
-            const paragraphNode = lex.$createParagraphNode();
-
-            // Create a new TextNode
-            const textNode = lex.$createTextNode("Hello world");
-
-            // Append the text node to the paragraph
-            paragraphNode.append(textNode);
-
-            // Finally, append the paragraph to the root
-            root.append(paragraphNode);
-            editor.setEditable(true);
-        });
-        editor.registerUpdateListener(({ editorState }) => {
-            editorState.read(() => {
-                const root = lex.$getRoot();
-                console.log(root.getTextContent());
-            });
-        });
+    const { editor, data, isReady } = createEditor({
+        tools: {
+            header: Header,
+        },
     });
+
+    $: {
+        if (isReady) {
+            console.log("Saved data:", $data);
+        }
+    }
+    onMount(() => {});
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>
     Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
-<div contenteditable="true" bind:this={editorElement}></div>
+<div class="editor" use:editor />
+
+<style>
+    .editor {
+        border: 2px solid #ccc;
+    }
+</style>
