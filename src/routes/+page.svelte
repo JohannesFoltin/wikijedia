efb38eeba15e695b980859740305724bba6c6d5a
Johannes Foltin <Johannes.foltin@gmx.de>
2024-01-27 15:53:29 +0100

    added edit button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,49 @@
 <script lang="js">
     import { onMount } from 'svelte';
     import markdownit from 'markdown-it';
+    import "../app.css";
 
-    let markdownText = '## markdown preview';
+    let markdownText = '# Markdown preview';
     let html = '';
+    let isEditing = false;
 
     onMount(() => {
         updatePreview();
     });
 
     function updatePreview() {
-       // const markdownIt = mdurl.require('markdown-it')();
         const md = markdownit({
             breaks: true,
         });
         html = md.render(markdownText);
     }
 
+    function toggleEditing() {
+        isEditing = !isEditing;
+    }
+
 </script>
 
-<style>
-    textarea {
-        width: 100%;
-        height: 200px;
-    }
-</style>
-
-<textarea bind:value={markdownText} on:input={updatePreview}></textarea>
-<div>
-    {@html html}
+<div class="flex h-screen">
+    {#if isEditing}
+        <div class="flex-1 p-10">
+            <textarea class="w-full h-full resize-none border-black border-2 rounded" bind:value={markdownText} on:input={updatePreview}></textarea>
+        </div>
+        <div class="flex-1 p-10 overflow-y-auto">
+            {@html html}
+        </div>
+    {:else}
+        <div class="flex-1 p-10">
+                <div class="flex-1 overflow-y-auto">
+                    {@html html}
+                </div>
+        </div>
+    {/if}
+    <button class="fixed bottom-4 right-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={toggleEditing}>
+        {#if isEditing}
+            Preview
+        {:else}
+            Edit
+        {/if}
+    </button>
 </div>
