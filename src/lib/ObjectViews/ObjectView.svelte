82ea62d6201ff5e70eeda4b23e5698dda020928b
Johannes Foltin <Johannes.foltin@gmx.de>
2024-02-25 14:54:16 +0100

    Add support for PDF files in ObjectView

--- src/lib/ObjectViews/ObjectView.svelte
+++ src/lib/ObjectViews/ObjectView.svelte
@@ -108,6 +108,10 @@
                 <MarkdownEditorField bind:data={object.Data} on:update={sendObjectToServer}></MarkdownEditorField>
             {:else if object.Type === "image/png"}
                 <PictureView data={$serverURL+"/object/data/"+object.ID}></PictureView>
+            {:else if object.Type === "application/pdf"}
+                <iframe src={$serverURL+"/object/data/"+object.ID} class="w-full h-full" title={object.Name}></iframe>
+            {:else}
+                <p>Not supported</p>
             {/if}
         </div>
     </div>
